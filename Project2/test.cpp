eb8b4aad69799adf6c9f4a5ad381ad68f5caf452
Matthew Li <m739li@uwaterloo.ca>
2023-02-06 13:41:17 -0500

    Deleted test file

--- Project2/test.cpp
+++
@@ -1,8 +0,0 @@
-#include <vector>
-#include <iostream>
-
-int main() {
-    std::vector<int> myVector(5, 0);
-    myVector.insert(myVector.begin() + 1, 5);
-    std::cout << myVector[1] << std::endl;
-}
